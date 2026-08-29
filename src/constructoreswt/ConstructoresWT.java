fb53d517dc951913853ca4e0d55c6d9b574f48eb
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-28 20:08:24 -0500

    Importacion de codigo MAIN proveniente de herencia. Contiene instanciaciones mediante setters

--- src/constructoreswt/ConstructoresWT.java
+++ src/constructoreswt/ConstructoresWT.java
@@ -6,7 +6,19 @@ public class ConstructoresWT {
 
     
     public static void main(String[] args) {
-       
+        //INSTANCIACION DE OBJETOS MEDIANTE SETTERS
+        Estudiante estudiante1 = new Estudiante();
+        estudiante1.setNombre("Wilmar Tovar");
+        estudiante1.setEdad(16);
+        estudiante1.setCarrera("Ingenieria de Software");
+        
+        Profesor profesor1 = new Profesor();
+        profesor1.setNombre("William Gil");
+        profesor1.setAsignatura("POO y metodologias de software");
+        System.out.println("IMPRESION DE OBJETOS CON INTANCIACION POR MEDIO DE SETTERS");  
+        System.out.println(" El estudiante " + estudiante1.getNombre() + " estudia " + estudiante1.getCarrera() );
+        System.out.println(" El profesor " + profesor1.getNombre() + " ensena " + profesor1.getAsignatura());
+        System.out.println("============================================================");
     }
     
 }
