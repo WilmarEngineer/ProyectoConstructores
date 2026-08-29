498a1ba21df69ee1237d8e422039059940a735cf
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-28 20:02:48 -0500

    Creacion de metodos constructores para Subclase Estudiante que hereda de Superclase Persona

--- src/constructoreswt/Estudiante.java
+++ src/constructoreswt/Estudiante.java
@@ -6,6 +6,30 @@ public class Estudiante extends Persona{
    
     private String carrera="";
     
+    public Estudiante(String nombre, int edad, String carrera) {
+    super(nombre, edad);
+    this.carrera = carrera;
+    }
+    public Estudiante(String nombre, int edad) {
+    super(nombre, edad);
+    }
+    public Estudiante(String nombre, String carrera) {
+    super(nombre);
+    this.carrera = carrera;
+    }
+    public Estudiante(String nombre) {
+    super(nombre);
+    }
+    public Estudiante(int edad, String carrera) {
+    super(edad);
+    this.carrera = carrera;
+    }
+    public Estudiante(int edad) {
+    super(edad);
+    }
+    public Estudiante() {
+    this.carrera = "Ninguna";
+    }
     public String getCarrera(){
         return carrera;
     }
