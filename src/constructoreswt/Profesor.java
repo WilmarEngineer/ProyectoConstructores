2df00c1b17ed93d6f7131f45c3cf2991701f7556
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-28 20:05:02 -0500

    Creacion de metodos constructores para Subclase Profesor que hereda de Superclase Persona

--- src/constructoreswt/Profesor.java
+++ src/constructoreswt/Profesor.java
@@ -5,7 +5,30 @@ package constructoreswt;
 public class Profesor extends Persona {
    
     private String asignatura="";
-    
+    public Profesor(String nombre, int edad, String asignatura) {
+    super(nombre, edad);
+    this.asignatura = asignatura;
+    }
+    public Profesor(String nombre, int edad) {
+    super(nombre, edad);
+    }
+    public Profesor(String nombre, String asignatura) {
+    super(nombre);
+    this.asignatura = asignatura;
+    }
+    public Profesor(String nombre) {
+    super(nombre);
+    }
+    public Profesor(int edad, String asignatura) {
+    super(edad);
+    this.asignatura = asignatura;
+    }
+    public Profesor(int edad) {
+    super(edad);
+    }
+    public Profesor() {
+    this.asignatura = "Ninguna";
+    }
     public String getAsignatura(){
         return asignatura;
     }
