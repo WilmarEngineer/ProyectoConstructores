77ea2b3a3aad01f62f5bc300cf24dbfdcd3afc48
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-28 20:15:36 -0500

    Creacion de objetos de tipo Estudiante y Profesor por medio de metodos constructores, usando diferentes atributos y ordenes

--- src/constructoreswt/ConstructoresWT.java
+++ src/constructoreswt/ConstructoresWT.java
@@ -15,6 +15,13 @@ public class ConstructoresWT {
         Profesor profesor1 = new Profesor();
         profesor1.setNombre("William Gil");
         profesor1.setAsignatura("POO y metodologias de software");
+        //INSTANCIACION DE OBJETOS MEDIANTE METODOS CONTRUCTORES
+        Estudiante estudiante2 = new Estudiante("Wilmar", 16);
+        Estudiante estudiante3 = new Estudiante(16, "Ingenieria de Software");
+        
+        Profesor profesor2 = new Profesor("William", "POO");
+        Profesor profesor3 = new Profesor("William");
+        
         System.out.println("IMPRESION DE OBJETOS CON INTANCIACION POR MEDIO DE SETTERS");  
         System.out.println(" El estudiante " + estudiante1.getNombre() + " estudia " + estudiante1.getCarrera() );
         System.out.println(" El profesor " + profesor1.getNombre() + " ensena " + profesor1.getAsignatura());
