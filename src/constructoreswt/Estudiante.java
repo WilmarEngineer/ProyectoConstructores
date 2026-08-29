69bded93fe7dbe7e300de68f0ca612a09539c5d6
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-28 19:51:28 -0500

    Importacion de Subclase estudiante proveniente de proyecto de herencia

---
+++ src/constructoreswt/Estudiante.java
@@ -0,0 +1,15 @@
+
+package constructoreswt;
+
+
+public class Estudiante extends Persona{
+   
+    private String carrera="";
+    
+    public String getCarrera(){
+        return carrera;
+    }
+    public void setCarrera(String carrera){
+        this.carrera = carrera;
+    }
+}
